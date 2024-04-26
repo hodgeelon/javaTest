8086df1db0ad213d6ff98bcb802d7fd2cd8fedb2
elon <--replace-all>
2024-04-26 15:28:48 -0400

    test1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+
     }
 
     public static void test(){
